92345f0160e5086b8ce3e0b9bffaaf7af808e91f
Likhon22 <likhonsarker793@gmail.com>
2024-06-08 15:28:32 +0600

    quantity and inStock logic updated

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -1,12 +1,46 @@
 import { Request, Response } from 'express';
 import { orderServices } from './order.service';
 import { orderValidation } from './order.validation';
+import { productServices } from '../product/product.service';
 
 const saveOrder = async (req: Request, res: Response) => {
   try {
     const order = req.body;
+    const { productId } = req.body;
+    const getProductById = await productServices.getSingleProduct(productId);
+
+    if (!getProductById) {
+      return res.status(404).json({
+        success: false,
+        message: 'Product not found!',
+      });
+    }
+    const quantityOfProduct = getProductById.inventory.quantity;
+    const quantityOfOrder = order.quantity;
+    // if quantity of order is greater than quantity of product then send invalid order
+    if (quantityOfOrder > quantityOfProduct) {
+      return res.status(400).json({
+        success: false,
+        message: 'Insufficient quantity available in inventory',
+      });
+    }
+
+    const newQuantity = quantityOfProduct - quantityOfOrder;
+
+    // if new quantity is zero then inStock is false
+    if (newQuantity === 0) {
+      getProductById.inventory.inStock = false;
+      await productServices.updateProductByIdFromDB(productId, getProductById);
+    }
+
+    // setting new quantity to the product
+    getProductById.inventory.quantity = newQuantity;
+
     const parsesZodOrderData = orderValidation.parse(order);
+    // get the order  result
     const result = await orderServices.saveOrderIntoDB(parsesZodOrderData);
+    // update the product
+    await productServices.updateProductByIdFromDB(productId, getProductById);
     res.status(200).json({
       success: true,
       message: 'Order created successfully!',
